bd4787763650eb05d3f4609a1007c747fb187534
Mikhail Kuznetsov <58913963+cyberic-icus@users.noreply.github.com>
2023-04-20 14:38:00 +0300

    Update Chrome.java

--- creational-design-patterns/src/main/java/org/example/factorymethod/Chrome.java
+++ creational-design-patterns/src/main/java/org/example/factorymethod/Chrome.java
@@ -2,16 +2,6 @@ package org.example.factorymethod;
 
 public class Chrome extends Browser {
     @Override
-    public boolean isOpenSource() {
-        return false;
-    }
-
-    @Override
-    public boolean hasPlugins() {
-        return true;
-    }
-
-    @Override
     public String renderHTML() {
         return "<html><body>Hello, Chrome!</body></html>";
     }
